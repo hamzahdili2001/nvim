ba30ded922778bd0d2084406ba30e1cbeb121122
hamzahdili2001 <hdilihamza955@gmail.com>
2023-03-07 15:43:15 +0100

    add config files

---
+++ lua/config/lazy.lua
@@ -0,0 +1,66 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  -- bootstrap lazy.nvim
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+
+require("lazy").setup({
+  spec = {
+    -- add LazyVim and import its plugins
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- import any extras modules here
+    -- { import = "lazyvim.plugins.extras.lang.typescript" },
+    -- { import = "lazyvim.plugins.extras.lang.json" },
+    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    -- import/override with your plugins
+    { import = "plugins" },
+  },
+  defaults = {
+    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+    lazy = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
+  },
+  install = { colorscheme = { "tokyonight", "habamax" } },
+  checker = { enabled = true }, -- automatically check for plugin updates
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
+
+
+-- Betty:
+--vim.g.betty_command = "/usr/local/bin/betty"
+
+-- define function to format code to Holberton School betty style
+--function format_to_holberton_betty()
+  --local command = string.format("%s --betty-style=hack %s", vim.g.betty_command, vim.fn.expand("%:p"))
+  --vim.fn.system(command)
+  --vim.api.nvim_command("e!")
+--end
+
+-- run format_to_holberton_betty() function when saving .c files
+--vim.api.nvim_exec([[
+  --augroup LazyVimBetty
+    --autocmd!
+    --autocmd BufWritePre *.c lua format_to_holberton_betty()
+  --augroup END
+--]], false)
+-- 
