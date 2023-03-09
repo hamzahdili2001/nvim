5492da02cc2e76db4fd24eb7771d4a6adc6f6947
hamzahdili2001 <hdilihamza955@gmail.com>
2023-03-09 17:52:09 +0100

    changing some conf

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,35 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+function CleanupFile()
+  -- Get the lines of the current buffer
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+
+  -- Iterate over each line
+  for i, line in ipairs(lines) do
+	-- Remove trailing whitespace
+	lines[i] = line:gsub("%s+$", "")
+
+	-- Convert consecutive groups of four spaces to a single tab character
+	lines[i] = lines[i]:gsub("	", "\t")
+  end
+
+  -- Set the lines of the current buffer to the cleaned up lines
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+end
+
+function DisableAutoComplete()
+  local filetype = vim.bo.filetype
+
+  if filetype ~= "c" then
+	vim.bo.omnifunc = ""
+	vim.bo.keywordprg = ""
+	vim.bo.path = ""
+	vim.g.tex_conceal = ""
+  else
+	vim.g.tex_conceal = "abdmg"
+  end
+end
+
+vim.cmd([[autocmd BufWritePre * lua CleanupFile()]])
+vim.cmd([[autocmd FileType * lua DisableAutoComplete()]])
+
